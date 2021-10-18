d5b8990037cd3e670d8aed70e60abbea0fa877f3
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2021-10-18 17:36:55 +0500

    ADDING LOCATION

--- DSSCODModel/src/pqt/dss/cod/model/eo/DssChangeOfDesignationImpl.java
+++ DSSCODModel/src/pqt/dss/cod/model/eo/DssChangeOfDesignationImpl.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpSession;
 import oracle.jbo.AttributeList;
 import oracle.jbo.Key;
 import oracle.jbo.RowIterator;
+import oracle.jbo.ViewObject;
 import oracle.jbo.domain.Char;
 import oracle.jbo.domain.Date;
 import oracle.jbo.domain.NClobDomain;
@@ -57,6 +58,7 @@ public class DssChangeOfDesignationImpl extends EntityImpl {
         CodDocNumber,
         WorkflowNotificationId,
         WorkflowStatus,
+        GisLocationIdFk,
         DssCodMgrRecruitmentForm,
         DssCodNewRecruitedMgr;
         private static AttributesEnum[] vals = null;
@@ -104,6 +106,7 @@ public class DssChangeOfDesignationImpl extends EntityImpl {
     public static final int CODDOCNUMBER = AttributesEnum.CodDocNumber.index();
     public static final int WORKFLOWNOTIFICATIONID = AttributesEnum.WorkflowNotificationId.index();
     public static final int WORKFLOWSTATUS = AttributesEnum.WorkflowStatus.index();
+    public static final int GISLOCATIONIDFK = AttributesEnum.GisLocationIdFk.index();
     public static final int DSSCODMGRRECRUITMENTFORM = AttributesEnum.DssCodMgrRecruitmentForm.index();
     public static final int DSSCODNEWRECRUITEDMGR = AttributesEnum.DssCodNewRecruitedMgr.index();
 
@@ -463,6 +466,22 @@ public class DssChangeOfDesignationImpl extends EntityImpl {
     }
 
     /**
+     * Gets the attribute value for GisLocationIdFk, using the alias name GisLocationIdFk.
+     * @return the value of GisLocationIdFk
+     */
+    public Number getGisLocationIdFk() {
+        return (Number) getAttributeInternal(GISLOCATIONIDFK);
+    }
+
+    /**
+     * Sets <code>value</code> as the attribute value for GisLocationIdFk.
+     * @param value value to set the GisLocationIdFk
+     */
+    public void setGisLocationIdFk(Number value) {
+        setAttributeInternal(GISLOCATIONIDFK, value);
+    }
+
+    /**
      * @return the associated entity oracle.jbo.RowIterator.
      */
     public RowIterator getDssCodMgrRecruitmentForm() {
@@ -500,13 +519,21 @@ public class DssChangeOfDesignationImpl extends EntityImpl {
          
 //         setBranchStatus("INCOMPLETE");
 //         setDssStatus("INCOMPLETE");
-         
+         ViewObject vo=getDBTransaction().getRootApplicationModule().findViewObject("DSSUserLocVO");
+                   if (vo!=null)
+            {
+                    vo.remove();
+            }
+
          FacesContext fctx = FacesContext.getCurrentInstance();
          ExternalContext ectx = fctx.getExternalContext();
          HttpSession userSession = (HttpSession) ectx.getSession(false);
          try {
              setUserIdFk(new Number(userSession.getAttribute("pUserId")));
              setLastUpdatedBy(new Number(userSession.getAttribute("pUserId")));
+             vo=getDBTransaction().getRootApplicationModule().createViewObjectFromQueryStmt("DSSUserLocVO", "select  GIS_LOCATION_ID_FK from DSS_SM_USERS WHERE USER_ID_PK="+getUserIdFk());
+             vo.executeQuery();             
+             setGisLocationIdFk(new Number( vo.first().getAttribute(0).toString() ) );
          } catch (SQLException ex) {
              setUserIdFk(new Number(0));
              setLastUpdatedBy(new Number(0));
